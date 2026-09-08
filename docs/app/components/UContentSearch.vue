ddca4b02781e5e7d3a8307f9bb53801d7f50313d
Eduardo San Martin Morote <posva@users.noreply.github.com>
2026-09-08 03:37:51 +0200

    docs: keep guides first in docs search (#367)

---
+++ docs/app/components/UContentSearch.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import ContentSearch from '@nuxt/ui/components/content/ContentSearch.vue'
+
+defineOptions({ inheritAttrs: false })
+</script>
+
+<template>
+  <ContentSearch
+    v-bind="$attrs"
+    preserve-group-order
+  />
+</template>
